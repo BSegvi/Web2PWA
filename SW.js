cfd6c70a8254f8f92380ee6a71c3b2e2e77e30d3
Brunosegvic <brunoojaa@gmail.com>
2023-01-02 00:32:50 +0100

    Clean up code after fixing cache

--- SW.js
+++ SW.js
@@ -1,31 +1,30 @@
 const staticCacheName = "site-static";
-import { del, entries } from "https://cdn.jsdelivr.net/npm/idb-keyval@6/+esm";
-import { get, set } from "https://cdn.jsdelivr.net/npm/idb-keyval@6/+esm";
 
 const assets = [
-  "/",
-  "/contact",
-  "/css/style.css",
-  "/index.html",
-  "/contact.html",
-  "/manifest.json",
-  "/favicon.ico",
-  "/images/guitar-icon.jpg",
-  "/images/guitar-icon(1).jpg",
-  "/images/guitar-icon(2).jpg",
+  "./",
+  "./contact",
+  "./css/style.css",
+  "./index.html",
+  "./contact.html",
+  "./manifest.json",
+  "./favicon.ico",
+  "./images/guitar-icon.jpg",
+  "./images/guitar-icon(1).jpg",
+  "./images/guitar-icon(2).jpg",
+  "./images/guitar-icon(3).jpg",
 
-  "/images/guitar-icon(4).jpg",
-  "/images/guitar-icon(5).jpg",
-  "/images/guitar-icon(7).jpg",
-  "/images/guitar-icon(8).jpg",
-  "/images/sweetHomeAlabama.png",
-  "/images/guitar.jpg",
-  "/images/SweetChildTab.jpg",
-  "/images/standbyme.JPG",
+  "./images/guitar-icon(4).jpg",
+  "./images/guitar-icon(5).jpg",
+  "./images/guitar-icon(7).jpg",
+  "./images/guitar-icon(8).jpg",
+  "./images/sweetHomeAlabama.png",
+  "./images/guitar.jpg",
+  "./images/SweetChildTab.jpg",
+  "./images/standbyme.JPG",
 ];
 
 self.addEventListener("install", (event) => {
-  //console.log("service worker has been installed");
+  console.log("service worker has been installed");
   event.waitUntil(
     caches.open(staticCacheName).then((cache) => {
       console.log("cashing shell assets");
