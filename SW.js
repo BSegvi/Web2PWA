f362e42d55f0d9bccfb8acfa16aebe06f3c58680
Brunosegvic <brunoojaa@gmail.com>
2023-01-02 02:45:59 +0100

    Finish

--- SW.js
+++ SW.js
@@ -46,7 +46,6 @@ self.addEventListener("activate", (event) => {
 
 //fetch event
 self.addEventListener("fetch", (event) => {
-  //console.log("fetch event", evt);
   event.respondWith(
     caches.match(event.request).then((cacheResponse) => {
       return cacheResponse || fetch(event.request);
@@ -56,7 +55,6 @@ self.addEventListener("fetch", (event) => {
 
 self.addEventListener("sync", (event) => {
   if (event.tag === "sync") {
-    //call method
     event.waitUntil(pushNotify());
   }
 });
@@ -67,14 +65,6 @@ self.addEventListener("sync", (event) => {
   }
 });
 function pushNotify() {
-  /*fetch("http://localhost:3000/contact", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(formData),
-  })
-    .then(() => Promise.resolve())
-    .catch(() => Promise.reject());*/
-
   self.registration.showNotification(`Message sent!`);
 }
 function pushNotifyTwo() {
